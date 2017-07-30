e819b263da85c2212a3f4b45e9d627198397b0b9
Shrinivas Bhat <bshinu51@gmail.com>
2017-07-30 14:33:55 -0700

    Jenkin Interface: handle jenkin adapter and test cases, if not then AutomationManager need to extract the test cases from xls and execute

---
+++ src/main/java/com/automation/JenkinAdapter.java
@@ -0,0 +1,31 @@
+package com.automation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * To trigger build from Jenkin this Adapter is used. Call main method with
+ * selected test cases if required. Or the framework will execute the default
+ * test cases extracted from the Automation.xls.
+ * 
+ * <p>
+ * <strong> Note that if any of the test case fails, then the system shall exit
+ * with failure, and generate report and stores the report into <em>reports</em>
+ * folder. If all the test cases pass, then system shall exit with
+ * success.</strong>
+ * 
+ * @author Shrinivas Bhat <sbhat10@asu.edu> <bshinu51@gmail.com>
+ * @version 1.0.0
+ */
+public class JenkinAdapter {
+
+	public static void main(String[] args) {
+		ArrayList<String> jenkinTestCases = new ArrayList<String>(
+				Arrays.asList(args));
+		AutomationManager.initTestExecution(jenkinTestCases);
+		if (AutomationManager.hasFailed()) {
+			System.exit(1);
+		}
+		System.exit(0);
+	}
+}
